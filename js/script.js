bbea372fd4f39135619035e9f5be6c7a583aef3c
Yiying Ding <yiyingd@andrew.cmu.edu>
2020-05-05 23:05:03 -0400

    Final Adjust ment

---
+++ js/script.js
@@ -0,0 +1,6 @@
+AOS.init();
+
+	    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                event.preventDefault();
+                $(this).ekkoLightbox();
+            });
